2225719d8e217bd61b2d471a5099d8197d254a2e
Sally <sallylau.tw@gmail.com>
2018-02-08 20:41:25 +0800

    Initial Commit

---
+++ SOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SOS
+//
+//  Created by Ga Wai Lau on 08/02/2018.
+//  Copyright © 2018 Ga Wai Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
